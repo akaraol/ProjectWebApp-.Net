40f57540cf288a80da1f83e99d8fbbd71eff69c3
Abdullah Karaoğul <90260540+akaraol@users.noreply.github.com>
2025-08-07 15:00:31 +0300

    Initial push
    mail kit added
    connection made
    Identity added

--- ProjectWebApp/obj/Release/net8.0/ProjectWebApp.AssemblyInfo.cs
+++ ProjectWebApp/obj/Release/net8.0/ProjectWebApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectWebApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44fd2a1ad0ca240542583fe248cb940071d7763")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectWebApp")]
